5417ed57b8af9c3092d08c5d9a750e54f5aaa90d
Malykh Oleg <malykholeg@gmail.com>
2011-07-04 17:28:41 +0400

    tune script

--- bin/set_width_and_height_to_pictures.rb
+++ bin/set_width_and_height_to_pictures.rb
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ruby18
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 
-Picture.find(:all, :conditions => ["photo_cached_width_original IS NULL"]).each {|picture|
+Picture.find(:all, :conditions => ["photo_cached_width_original IS NULL OR photo_cached_width_resized IS NULL"]).each {|picture|
   picture.set_width_and_height
   picture.save!
 }
